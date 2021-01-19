b12c5cd0f95c0b9c2de5c24f4a4b23130e3d8358
Roman Turner <rn.turner@live.com>
2021-01-19 02:21:32 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -11,13 +11,10 @@ loop_message_five_times("Hello World")
 
 def loop_message_n_times(str_var, n)
   counter = 0
-  if n < 0 
-   break
   else 
    while counter < n do
     puts str_var
     counter += 1 
-  end 
  end 
 end  
 loop_message_n_times("Hello Moon", 5)
